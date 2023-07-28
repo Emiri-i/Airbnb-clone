17507930bd8c69e7c1e378c69dc2714e87bf7787
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-07-28 21:21:01 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>I am the header</h1>
+      <div></div>
+
+      {/* <div></div>
+      <div></div> */}
+    </header>
+  );
+}
+
+export default Header;
