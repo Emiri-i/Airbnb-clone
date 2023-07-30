3528536481025f72afdaed48a6c35e240e1593b5
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-07-30 21:10:17 +0200

    created banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
-
+import Banner from "@/components/Banner";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -11,7 +11,7 @@ export default function Home() {
         <Header></Header>
 
         {/* Banner */}
-
+        <Banner />
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.tsx</code>
